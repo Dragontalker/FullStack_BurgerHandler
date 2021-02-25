3f49d4754dd0de75ad8faa2fe23d720c6e77cdbc
Richard <richard.yang.tong@gmail.com>
2021-02-25 14:29:56 -0500

    Created a mysql seed file.

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Big Mac', true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Quarter Pounder', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Double Cheese Burger', false);

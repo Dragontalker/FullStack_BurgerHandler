9cb318d6aa55d716ffc62dccb73077277d7fc74d
Richard <richard.yang.tong@gmail.com>
2021-02-25 16:56:04 -0500

    Define a connection config setting for mysql database.

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,9 @@
 const Sequalize = require('sequalize');
 require('dotenv').config()
 
-console.log(process.env.SQL_PASSWORD);
+const sequalize = new Sequalize('burgers_db', 'root', process.env.SQL_PASSWORD, {
+    host: 'localhost',
+    port: 3306,
+    dialect: mysql,
+    pool: {max: 5, min: 0, idel: 10000}
+});

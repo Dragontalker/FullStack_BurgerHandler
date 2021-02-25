fba4bf54b7d72bd91e5cef994c8ec4213f11bee0
Richard <richard.yang.tong@gmail.com>
2021-02-25 17:31:59 -0500

    Tested the connectin with sequalize, passed.

--- config/connection.js
+++ config/connection.js
@@ -8,10 +8,27 @@ const sequelize = new Sequelize('burgers_db', 'root', process.env.SQL_PASSWORD, 
     pool: {max: 5, min: 0, idel: 10000}
 });
 
-sequelize
+const Burger = sequelize.define('burgers', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    burger_name: Sequelize.STRING,
+    devoured: Sequelize.BOOLEAN,
+    createdAt: {
+        field: 'created_at',
+        type: Sequelize.DATE
+    },
+    updatedAt: {
+        field: 'updated_at',
+        type: Sequelize.DATE
+    }
+});
+
+Burger
     .sync({
         logging: console.log,
-        force: false
+        force: true
     })
     .then(() => {
         console.log('Connection to database established successfully');
@@ -19,3 +36,18 @@ sequelize
     .catch(err => {
         console.error('Unable to connect to the database: ', err);
     });
+
+
+// Test 
+const main = async () => {
+    const burgerList = await Burger.findAll({})
+    
+    console.log( `\n\n--------------------------` )
+    burgerList.forEach( burger =>{
+        console.log( `#${burger.id}: Burger: "${burger.burger_name}", Devour Status: ${burger.devoured}` );
+    });
+
+    sequelize.close()
+};
+
+main()

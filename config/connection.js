e45cd29798b39b62721407794d4157de2c5a15cb
Richard <richard.yang.tong@gmail.com>
2021-02-26 08:35:33 -0500

    Added a condition for constructor for future deployment on Heroku.

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const myseql = require('mysql');
 
 class Database {
     constructor(config) {
-        this.connection = mysql.createConnection(config);
+        this.connection = mysql.createConnection( process.env.JAWSDB_URL ? process.env.JAWSDB_URL : config );
     }
 };
 

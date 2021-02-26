00ba3c289fabf90893dfab76cb240bcf3b84db72
Richard <richard.yang.tong@gmail.com>
2021-02-26 12:54:07 -0500

    Cleaned up all the unused comments and ready to export.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Burger = require('../models/burgers');
 
-// Get burger list
 router.get('/api', (req, res) => {
     Burger.listAll()
         .then(burger => res.json(burger))
@@ -20,66 +19,5 @@ router.put('/api/devour', (req, res) => {
         .then(burger => console.log(`Successfuly devoured ${req.body.id}!`))
         .catch(err => console.log(err));
 });
-
-
-
-
-// Add a BigMac
-// router.get('/bigmac', (req, res) => {
-//     const data = {
-//         burger_name: 'Big Mac',
-//         devoured: true
-//     };
-
-//     let { burger_name, devoured } = data;
-
-//     // Insert into table
-//     Burger.create({
-//         burger_name: burger_name,
-//         devoured: devoured
-//     })
-//         .then(burger => res.redirect('/burgers'))
-//         .catch(err => console.log(err));
-
-// });
-
-// // Add a QuarterPounder
-// router.get('/quarterpounder', (req, res) => {
-//     const data = {
-//         burger_name: 'Quarter Pounder',
-//         devoured: false
-//     };
-
-//     let { burger_name, devoured } = data;
-
-//     // Insert into table
-//     Burger.create({
-//         burger_name: burger_name,
-//         devoured: devoured
-//     })
-//         .then(burger => res.redirect('/burgers'))
-//         .catch(err => console.log(err));
-
-// });
-
-// // Add a DoubleCheese
-// router.get('/doublecheese', (req, res) => {
-//     const data = {
-//         burger_name: 'Double Cheese',
-//         devoured: false
-//     };
-
-//     let { burger_name, devoured } = data;
-
-//     // Insert into table
-//     Burger.create({
-//         burger_name: burger_name,
-//         devoured: devoured
-//     })
-//         .then(burger => res.redirect('/burgers'))
-//         .catch(err => console.log(err));
-
-// });
-
 
 module.exports = router;

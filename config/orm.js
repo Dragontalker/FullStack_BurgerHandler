13a8b58babe44851945c8ada869be6f68c222eec
Richard <richard.yang.tong@gmail.com>
2021-02-26 10:19:01 -0500

    Defined the three methods we need to add into orm object.

--- config/orm.js
+++ config/orm.js
@@ -2,7 +2,17 @@ const db = require('./connection')('burgers_db', process.env.SQL_PASSWORD);
 require('dotenv').config()
 
 const orm = {
+    selectAll() {
 
+    },
+
+    insertOne() {
+
+    },
+
+    updateOne() {
+        
+    }
 };
 
 module.exports = orm;

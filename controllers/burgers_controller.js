270ef09d0fbd8b2496f3f03d488f2d69dd3d6ae5
Richard <richard.yang.tong@gmail.com>
2021-02-25 22:44:36 -0500

    Improved the router to send response from database.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../config/connection');
+const Burgers = require('../models/burgers');
 
 router.get('/', (req, res) => {
-    res.send('BURGERS');
+    Burgers.findAll()
+        .then(burger => {
+            console.log(burger);
+            res.sendStatus(200);
+        })
+        .catch(err => console.log(err));
 });
 
 module.exports = router;

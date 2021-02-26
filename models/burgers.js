6888705394989090227dee4b248df8473f680aec
Richard <richard.yang.tong@gmail.com>
2021-02-26 10:53:39 -0500

    Set up all the methods in the burger model.

--- models/burgers.js
+++ models/burgers.js
@@ -1,7 +1,19 @@
 const orm = require('../config/orm');
 
 const burger = {
+    name: 'burgers_db',
 
+    listAll: async () => {
+
+    },
+
+    cookNew: async (burgerName) => {
+
+    },
+
+    devourThis: async (burgerID) => {
+
+    }
 };
 
 module.exports = burger;

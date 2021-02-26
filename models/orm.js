5ecb47746a9d0d07c43dc7af147e00c2faa1be3b
Richard <richard.yang.tong@gmail.com>
2021-02-25 22:32:13 -0500

    Modified our data model and exported it.

--- models/orm.js
+++ models/orm.js
@@ -1,19 +1,9 @@
 const Sequelize = require('sequelize');
-const db = require('./config/connection')
+const db = require('../config/connection');
 
 const Burger = db.define('burgers', {
-    id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true
-    },
     burger_name: Sequelize.STRING,
-    devoured: Sequelize.BOOLEAN,
-    createdAt: {
-        field: 'created_at',
-        type: Sequelize.DATE
-    },
-    updatedAt: {
-        field: 'updated_at',
-        type: Sequelize.DATE
-    }
+    devoured: Sequelize.BOOLEAN
 });
+
+module.exports = Burger;

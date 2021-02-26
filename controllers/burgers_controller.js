8c0442fc51c18429e8106436ba43b191e0983058
Richard <richard.yang.tong@gmail.com>
2021-02-25 22:53:48 -0500

    Created a simple get route to add test data to our database.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../config/connection');
-const Burgers = require('../models/burgers');
+const Burger = require('../models/burgers');
 
+// Get burger list
 router.get('/', (req, res) => {
-    Burgers.findAll()
+    Burger.findAll()
         .then(burger => {
             console.log(burger);
             res.sendStatus(200);
         })
         .catch(err => console.log(err));
+});
+
+// Add a burger
+router.get('/add', (req, res) => {
+    const data = {
+        burger_name: 'Big Mac',
+        devoured: true
+    };
+
+    let { burger_name, devoured } = data;
+
+    // Insert into table
+    Burger.create({
+        burger_name: burger_name,
+        devoured: devoured
+    })
+        .then(burger => res.redirect('/burgers'))
+        .catch(err => console.log(err));
+
 });
 
 module.exports = router;

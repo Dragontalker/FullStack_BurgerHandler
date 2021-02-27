b71922d75665ab14921fd2c96e9867beff2a8bc9
Richard <richard.yang.tong@gmail.com>
2021-02-26 19:44:29 -0500

    Added the jQuery script for button event listeners.

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,25 @@
 $(document).ready(() => {
-    
+    $('#submit').on('click', () => {
+        const input = $('#burger_name').val().trim();
+        const newBurger = { burger_name: input };
+        $.ajax('/api/cook', {
+            type: 'POST',
+            data: newBurger
+        }).then(() => {
+            console.log(`Cooked a new ${input} burger!`);
+            location.reload();
+        }).catch(error => console.log(error));
+    });
+
+    $('.devour').on('click', () => {
+        const burgerID = $(this).data('id');
+        const index = { id: burgerID };
+        $.ajax('api/devour', {
+            type: 'PUT',
+            data: index
+        }).then(() => {
+            console.log(`Devoured the burger with id ${burgerID}!`);
+        }).catch(error => console.log(error));
+    });
+
 });
